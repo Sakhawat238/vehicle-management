baba5388140ad6bf8d945842fe4cb808ed75568b
unknown <sakhawath98@gmail.com>
2024-01-22 19:40:24 +0600

    gg

--- website/landingpage/urls.py
+++ website/landingpage/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import landingpage
+from .views import (landingpage, aboutpage, termspage)
 
 urlpatterns = [
-    path('', landingpage, name="landingpage")
+    path('', landingpage, name="landingpage"),
+    path('about-us/', aboutpage, name="aboutpage"),
+    path('terms-and-conditions/', termspage, name="termspage")
 ]
--- website/landingpage/views.py
+++ website/landingpage/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 def landingpage(request):
     context = {}
     return render(request, 'web/landingpage.html', context)
+
+
+def aboutpage(request):
+    context = {}
+    return render(request, 'web/whoweare.html', context)
+
+
+def termspage(request):
+    context = {}
+    return render(request, 'web/terms.html', context)
